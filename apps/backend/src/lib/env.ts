20ba16549ab07287199eaafae4ed666f01602437
Eli Edds <eliedds@gmail.com>
2025-02-08 14:51:01 -0500

    feat: make env req

--- apps/backend/src/lib/env.ts
+++ apps/backend/src/lib/env.ts
@@ -2,8 +2,8 @@ import { z } from "zod";
 import "dotenv/config";
 
 const envSchema = z.object({
-  PORT: z.number().positive().optional(),
-  DATABASE_URL: z.string().url().optional(),
+  PORT: z.number().positive(),
+  DATABASE_URL: z.string().url(),
 });
 
 export const env = envSchema.parse(process.env);
