3409675b3412f4d23b37fbe73a259faebdd4e3dc
Erik Svanes <epsvanes@ncsu.edu>
2025-02-08 14:38:40 -0500

    added the tabs

--- apps/mobile/app/(tabs)/_layout.tsx
+++ apps/mobile/app/(tabs)/_layout.tsx
@@ -2,9 +2,36 @@ import { Tabs } from 'expo-router';
 
 export default function TabLayout() {
   return (
-    <Tabs>
-      <Tabs.Screen name="index" />
-      <Tabs.Screen name="settings" />
+    <Tabs screenOptions={{ headerShown: false }}>
+      <Tabs.Screen 
+        name="index" 
+        options={{
+
+          title: 'Budget',
+          href: '/'
+        }} 
+      />
+      <Tabs.Screen 
+        name="quests"
+        options={{
+          title: 'Quests',
+          href: '/quests'
+        }}
+      />
+      <Tabs.Screen 
+        name="party"
+        options={{
+          title: 'Party',
+          href: '/party'
+        }}
+      />
+      <Tabs.Screen 
+        name="leaderboard"
+        options={{
+          title: 'Leaderboard',
+          href: '/leaderboard'
+        }}
+      />
     </Tabs>
   );
 }
