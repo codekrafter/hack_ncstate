b71a07cefcbc87eb945810ff0c3349b97a755fcd
Eli Edds <eliedds@gmail.com>
2025-02-09 09:44:40 -0500

    feat: cors

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -2,9 +2,16 @@ import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 import { env } from "./lib/env";
 import auth from "./routes/auth";
-import { authenticated } from "./lib/auth";
+import { cors } from "hono/cors";
 
-const app = new Hono().route("/auth", auth);
+const app = new Hono()
+  .use(
+    cors({
+      origin: ["localhost:8081"],
+      credentials: true,
+    })
+  )
+  .route("/auth", auth);
 
 console.log(`Server is running on http://localhost:${env.PORT}`);
 
